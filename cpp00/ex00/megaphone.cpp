15996bb14cc9fde19effcb8cde7937c558e50d28
RedJocker <mauricio.guaruja@gmail.com>
2024-12-13 18:15:37 -0300

    Feat: ex00 cpp00 megaphone

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,44 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   megaphone.cpp                                      :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2024/12/12 17:38:34 by maurodri          #+#    #+#             //
+//   Updated: 2024/12/13 18:15:20 by maurodri         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+std::string& str_transform(std::string &str, int (*char_fun) (int c))
+{
+	size_t	j;
+
+	j = -1;
+	while (++j < str.length())
+		str.at(j) = char_fun(static_cast<unsigned char>(str.at(j)));
+	return str;
+}
+
+int main(int argc, char *argv[])
+{
+  int			i;
+  std::string 	str;
+
+  if (argc == 1)
+  {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+  }
+  i = 0;
+  while (++i < argc)
+  {
+	str = argv[i];
+	std::cout << str_transform(str, std::toupper);;
+  }
+  return (0);
+}
