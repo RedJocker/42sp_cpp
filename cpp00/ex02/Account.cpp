5e3001d6a0c4059ea04978f02a5776fb6e9d37a5
redjocker <mauricio.guaruja@gmail.com>
2025-01-17 16:54:49 -0300

    Init: trivial implementation of required interfaces

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,68 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   Account.cpp                                        :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2025/01/17 16:22:54 by maurodri          #+#    #+#             //
+//   Updated: 2025/01/17 16:47:52 by maurodri         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Account.hpp"
+
+int Account::getNbAccounts()
+{
+	return 0;
+}
+
+int Account::getTotalAmount(void)
+{
+	return 0;
+}
+
+int Account::getNbDeposits(void)
+{
+	return 0;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return 0;
+}
+
+void Account::displayAccountsInfos(void)
+{
+	return;
+}
+
+Account::Account(int initial_deposit)
+{
+	return ;
+}
+
+Account::~Account(void)
+{
+	return ;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	return ;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	return false;
+}
+
+int Account::checkAmount(void) const
+{
+	return 0;
+}
+
+void Account::displayStatus(void) const
+{
+	return ;
+}
