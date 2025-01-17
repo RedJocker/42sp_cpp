ae9d15c0023f02c2333a7766be9d43a3c225ddf1
redjocker <mauricio.guaruja@gmail.com>
2025-01-17 19:01:04 -0300

    Feat: ex2 Account implementation

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,57 +6,127 @@
 //   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/01/17 16:22:54 by maurodri          #+#    #+#             //
-//   Updated: 2025/01/17 17:02:04 by maurodri         ###   ########.fr       //
+//   Updated: 2025/01/17 18:58:31 by maurodri         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include "Account.hpp"
+#include <ctime>
 #include <iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void	Account::_displayTimestamp(void)
+{
+	char time_str[25];
+	
+	std::time_t result = std::time(nullptr);
+	std::strftime(time_str, 50, "[%Y%m%d_%H%M%S]", std::localtime(&result));
+	std::cout << time_str;
+	//std::cout << "[19920104_091532]";
+}
 
 int Account::getNbAccounts()
 {
-	return 0;
+	return _nbAccounts;
 }
 
 int Account::getTotalAmount(void)
 {
-	return 0;
+	return _totalAmount;
 }
 
 int Account::getNbDeposits(void)
 {
-	return 0;
+	return _totalNbDeposits;
 }
 
 int Account::getNbWithdrawals(void)
 {
-	return 0;
+	return _totalNbWithdrawals;
 }
 
 void Account::displayAccountsInfos(void)
 {
-	std::cout << "Hello" << std::endl;
+	Account::_displayTimestamp();
+	std::cout << " accounts:"<< getNbAccounts() << ";"
+			  << "total:" << getTotalAmount() << ";"
+			  << "deposits:" << getNbDeposits() << ";"
+			  << "withdrawals:" << getNbWithdrawals() << std::endl;
 	return;
 }
 
-Account::Account(int initial_deposit)
+Account::Account(int initial_deposit) :
+	_accountIndex(Account::_nbAccounts),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
 {
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+	Account::_displayTimestamp();
+	std::cout << " index:"<< _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "created" << std::endl;
 	return ;
 }
 
 Account::~Account(void)
 {
+	Account::_displayTimestamp();
+	std::cout << " index:"<< _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "closed" << std::endl;
 	return ;
 }
 
 void Account::makeDeposit(int deposit)
 {
+	const int previous_amount = this->_amount;
+
+	this->_nbDeposits++;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";"
+			  << "p_amount:" << previous_amount << ";"
+			  << "deposit:" << deposit << ";"
+			  << "amount:" << _amount << ";"
+			  << "nb_deposits:" << _nbDeposits
+			  << std::endl;
 	return ;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-	return false;
+	const int new_amount = this->_amount - withdrawal;
+	Account::_displayTimestamp();
+	if (new_amount < 0)
+	{
+		std::cout << " index:" << _accountIndex << ";"
+				  << "p_amount:" << this->_amount << ";"
+				  << "withdrawal:refused"
+				  << std::endl;
+		return false;
+	}
+
+	this->_nbWithdrawals++;
+	Account::_totalNbWithdrawals++;
+	
+	std::cout <<  " index:" << _accountIndex << ";"
+			  << "p_amount:" << this->_amount << ";"
+			  << "withdrawal:" << withdrawal << ";"
+			  << "amount:" << new_amount << ";"
+			  << "nb_withdrawals:" << 1
+		<< std::endl;
+	
+	this->_amount -= withdrawal;
+	Account::_totalAmount -= withdrawal;
+	return true;
 }
 
 int Account::checkAmount(void) const
@@ -66,5 +136,12 @@ int Account::checkAmount(void) const
 
 void Account::displayStatus(void) const
 {
+	Account::_displayTimestamp();
+	std::cout << " index:"<< _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "deposits:" << _nbDeposits << ";"
+			  << "withdrawals:" << _nbWithdrawals
+			  << std::endl;
+	
 	return ;
 }
