b7c631e34dc0ecc1f6a22ee37554f360081db84f
redjocker <mauricio.guaruja@gmail.com>
2025-01-17 19:59:51 -0300

    Init: create scafold based on requirements cpp01 ex00 Zombie

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,30 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   Zombie.cpp                                         :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2025/01/17 19:45:51 by maurodri          #+#    #+#             //
+//   Updated: 2025/01/17 20:10:41 by maurodri         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Zombie.hpp"
+#include <string>
+#include <iostream>
+
+void Zombie::announce(void)
+{
+	return ;
+}
+
+Zombie::Zombie(std::string name) : name(name)
+{
+
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Destroy " << this->name << std::endl;
+}
---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,47 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   Zombie.hpp                                         :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2025/01/17 19:46:04 by maurodri          #+#    #+#             //
+//   Updated: 2025/01/17 20:10:14 by maurodri         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+#endif
+
+// First, implement a Zombie class. It has a string private attribute name.
+// Add a member function void announce( void ); to the Zombie class. Zombies
+// announce themselves as follows:
+// <name>: BraiiiiiiinnnzzzZ...
+// Don’t print the angle brackets (< and >). For a zombie named Foo, the message
+// would be:
+// Foo: BraiiiiiiinnnzzzZ...
+class Zombie {
+	std::string name;
+public:
+	void announce(void);
+	Zombie(std::string name);
+	~Zombie();
+};
+
+// Then, implement the two following functions:
+
+// • Zombie* newZombie( std::string name );
+// It creates a zombie, name it, and return it so you can use it outside of the function
+// scope.
+Zombie* newZombie( std::string name );
+// • void randomChump( std::string name );
+// It creates a zombie, name it, and the zombie announces itself.
+void randomChump( std::string name );
+
+// Now, what is the actual point of the exercise? You have to determine in what case
+// it’s better to allocate the zombies on the stack or heap.
+// Zombies must be destroyed when you don’t need them anymore. The destructor must
+// print a message with the name of the zombie for debugging purposes.
