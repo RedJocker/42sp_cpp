138b9ffdaa4cb5bc291f3a16bf60db82884137cb
redjocker <mauricio.guaruja@gmail.com>
2025-07-02 23:46:34 -0300

    Fix: remove debug print on argc state

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 //   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/02/03 15:58:31 by maurodri          #+#    #+#             //
-//   Updated: 2025/07/01 19:24:10 by maurodri         ###   ########.fr       //
+//   Updated: 2025/07/02 23:33:43 by maurodri         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -22,8 +22,6 @@ int main(int argc, char *argv[])
 		std::cout << "Usage: converter strToConvert" << std::endl;
 		return 2;
 	}
-
-	std::cout << "argc: " << argc << std::endl;
 
 	std::string toConvert = argv[1];
 	ScalarConverter::convert(toConvert);
