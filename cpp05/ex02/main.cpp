8ec9211985a994b025f47ab469c17ae8abc9f427
RedJocker <mauricio.guaruja@gmail.com>
2025-07-16 17:22:06 -0300

    Reformat: cpp05 ex02: improve main before delivering

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 //   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/02/03 15:58:31 by maurodri          #+#    #+#             //
-//   Updated: 2025/07/01 17:12:48 by maurodri         ###   ########.fr       //
+//   Updated: 2025/07/16 17:21:11 by maurodri         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -19,11 +19,9 @@
 
 int	main(void)
 {
-	std::cout << std::endl << std::endl << std::endl;
+	std::cout << std::endl << "==A==" << std::endl;
 	Bureaucrat mrYes("Mr. Yes", 2);
 	Bureaucrat cantMan("Can't man", 100);
-
-	//
 	ShrubberyCreationForm shrubberryForm("restaurant");
 
 	mrYes.executeForm(shrubberryForm);
@@ -32,9 +30,9 @@ int	main(void)
 	std::cout << shrubberryForm << std::endl;
 	mrYes.executeForm(shrubberryForm);
 
-	std::cout << std::endl << std::endl << std::endl;
-	//
 
+	//
+	std::cout << std::endl << "==B==" << std::endl;
 	RobotomyRequestForm  robotomyForm("Can't man");
 
 	mrYes.executeForm(robotomyForm);
@@ -43,9 +41,9 @@ int	main(void)
 	std::cout << robotomyForm << std::endl;
 	mrYes.executeForm(robotomyForm);
 
-	std::cout << std::endl << std::endl << std::endl;
+	
 	//
-
+	std::cout << std::endl << "==C==" << std::endl;
 	PresidentialPardonForm presidentialForm("Civilian Protesting");
 	cantMan.signForm(presidentialForm);
 	cantMan.executeForm(presidentialForm);
