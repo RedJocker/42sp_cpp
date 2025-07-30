30add48181e61be481f727d5afc82d594f2a592c
RedJocker <mauricio.guaruja@gmail.com>
2025-07-30 20:52:49 -0300

    Wip: cpp09 ex02: add tests from generated tests that failed

--- cpp09/ex02/test.sh
+++ cpp09/ex02/test.sh
@@ -98,3 +98,53 @@ command=("$program" "30" "10" "50" "60" "40" "80" "20" "90" "70" )
 expected="10 20 30 40 50 60 70 80 90"
 expected_status='0'
 run_test_command
+
+command=("$program" 88 40 69 95 13 7 61 59)
+expected="7 13 40 59 61 69 88 95"
+expected_status='0'
+run_test_command
+
+command=("$program" 59 89 83 75 56 45 14 59 42)
+expected="14 42 45 56 59 59 75 83 89"
+expected_status='0'
+run_test_command
+
+command=("$program" 18 21 91 0 31 42 76 19 86 79)
+expected="0 18 19 21 31 42 76 79 86 91"
+expected_status='0'
+run_test_command
+
+command=("$program" 86 29 69 64 11 10 81 47 45 14)
+expected="10 11 14 29 45 47 64 69 81 86"
+expected_status='0'
+run_test_command
+
+command=("$program" 74 49 1 65 72 18 67 66)
+expected="1 18 49 65 66 67 72 74"
+expected_status='0'
+run_test_command
+
+command=("$program" 75 51 2 93 63 31 28 18 33)
+expected="2 18 28 31 33 51 63 75 93"
+expected_status='0'
+run_test_command
+
+command=("$program" 52 97 40 41 83 31 19 26 83 33)
+expected="19 26 31 33 40 41 52 83 83 97"
+expected_status='0'
+run_test_command
+
+
+# for size in {2..10}; do
+#     for run in {1..5}; do  # 5 tests per size
+# 	numbers=();
+# 	for ((i=0; i < size; i++)); do
+#             numbers+=($((RANDOM % 100)) )
+# 	done
+#         expected=$(printf "%s\n" "${numbers[@]}" | sort -n | xargs)
+        
+#         command=("$program" "${numbers[@]}")
+#         expected_status='0'
+#         run_test_command
+#     done
+# done
